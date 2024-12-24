30c9dbcba7ccdbccd8d0eb57c74a8e09fe09daab
Ambaks <78621642+Ambaks@users.noreply.github.com>
2024-12-23 20:08:12 -0500

    Updates

--- backend/models.py
+++
@@ -1,9 +0,0 @@
-from database import Base
-from sqlalchemy import Column, String, Integer
-
-class Email(Base):
-    __tablename__ = "emails"
-
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String)
-
